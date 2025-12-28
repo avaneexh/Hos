a30fa79e1abc5d933553276114c78ce255d5162a
avaneesh <avaneesh.gupta5@gmail.com>
2025-12-28 23:48:52 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,28 +7,31 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-[#f7f7f7] dark:bg-black'>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          
+        </Route>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/dashboard" replace />}
+        />
+
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/dashboard" replace />}
+        />
+        
+        
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/"
+          />
+        </Route>
+        
+      </Routes>
+    </div>
   )
 }
 
