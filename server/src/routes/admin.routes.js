8fa41c0a8b0f969a5e4be7764870b12d85afe319
avaneesh <avaneesh.gupta5@gmail.com>
2026-01-16 18:53:24 +0530

    updated sih routes

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,16 +1,20 @@
 import express from "express"
 import { authMiddleware, requireRole } from "../middleware/auth.middleware.js";
-import { addDish, changeDishAvailability, editDish, toggleDishInMenu } from "../controllers/admin.controller.js";
+import { addDish, changeDishAvailability, deleteDish, editDish, getAllCategories, toggleDishInMenu } from "../controllers/admin.controller.js";
 import { upload } from "../middleware/upload.middleware.js";
 
 
 const adminRoutes = express.Router();
 
 
-adminRoutes.post("/addDish", authMiddleware, requireRole("ADMIN"), upload.single("image"), addDish )
-adminRoutes.post("/editDish", authMiddleware, requireRole("ADMIN"), editDish )
-adminRoutes.post("/changeDishAvailability", authMiddleware, requireRole("ADMIN"), changeDishAvailability )
-adminRoutes.post("/toggleDishInMenu", authMiddleware, requireRole("ADMIN"), toggleDishInMenu )
+adminRoutes.post("/addDish", authMiddleware, requireRole("ADMIN"), upload.single("image"), addDish);
+adminRoutes.put("/dish/:id", authMiddleware, requireRole("ADMIN"), upload.single("image"), editDish);
+adminRoutes.patch("/dish/:id/availability", authMiddleware, requireRole("ADMIN"), changeDishAvailability);
+adminRoutes.patch("/dish/:id/inMenu", authMiddleware, requireRole("ADMIN"), toggleDishInMenu);
+adminRoutes.patch("/dish/:id/delete", authMiddleware, requireRole("ADMIN"), deleteDish);
+adminRoutes.get("/categories", authMiddleware, requireRole("ADMIN"), getAllCategories);
+
+
 
 
 export default adminRoutes;
